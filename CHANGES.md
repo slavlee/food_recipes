e0fea311e18cffa2bf5fe3541cdee4a9850b241a
slavlee <info@slavlee.de>
2025-04-24 23:37:34 +0200

    - [IMPORTANT] Intitial commmit - [FEATURE] Add composer.json - [FEATURE] Add ext_emconf.php

---
+++ CHANGES.md
@@ -0,0 +1,4 @@
+## Version dev-1.0.0-0
+- [IMPORTANT] Intitial commmit
+- [FEATURE] Add composer.json
+- [FEATURE] Add ext_emconf.php
---
+++ ext_emconf.php
@@ -0,0 +1,26 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Food Recipes',
+    'description' => 'TYPO3 Extension to create a food recipe database. Perfect for food blogs.',
+    'category' => 'misc',
+    'author' => 'Kevin Chileong Lee',
+    'author_email' => 'info@slavlee.de',
+    'author_company' => 'Slavlee',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+            'fluid_styled_content' => '13.4.0-13.4.99'
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Slavlee\\FoodRecipes\\' => 'Classes'
+        ]
+    ],
+];
